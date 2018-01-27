0aee339c303687ff466104987686edbd04e677e4
Laptop\Wayne <waynehelley@outlook.com>
2018-01-27 18:12:34 +0000

    LevelComplete logic.  Audio tags added.

--- Assets/_Scripts/LevelComplete.cs
+++ Assets/_Scripts/LevelComplete.cs
@@ -2,18 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
 
 public class LevelComplete : MonoBehaviour {
 
     private GameObject canvas;
-
+    private GameObject music;
+    private GameObject levelCompleteSound;
+     
     // Use this for initialization
     void Start () {
         canvas = GameObject.FindGameObjectWithTag("Canvas");
+        music = GameObject.FindGameObjectWithTag("Music");
+        levelCompleteSound = GameObject.FindGameObjectWithTag("LevelCompleteSound");
     }
 		
 	public void ActivateLevelComplete ()
     {
         canvas.GetComponentInChildren<Text>().enabled = true;
-	}
+
+        music.GetComponent<AudioSource>().Stop();
+        levelCompleteSound.GetComponent<AudioSource>().Play();
+
+        StartCoroutine(FadePause("Next", 2.0f));
+
+    }
+
+    IEnumerator FadePause(string SceneToChangeTo, float fadeTime)
+    {
+        Debug.Log("Fade Started");
+        yield return new WaitForSeconds(fadeTime);
+        Debug.Log("Countdown Done");
+
+        if(SceneManager.sceneCount< SceneManager.GetActiveScene().buildIndex)      
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            canvas.GetComponentInChildren<Text>().text = "Thanks for playing";
+        }
+
+    }
+
 }
