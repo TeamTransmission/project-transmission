1c1a1da49f155345c3afaec1a7903a10fdeea0a5
Laptop\Wayne <waynehelley@outlook.com>
2018-01-27 22:03:22 +0000

    Triggered Moving platform functioning.

---
+++ Assets/_Scripts/TriggeredMovingPlatform.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggeredMovingPlatform : MonoBehaviour
+{
+
+    private GameObject player;
+
+    public int Marker = 0;
+    private int WayPointLength;
+    public int speed = 4;
+    public Transform[] Waypoints;
+    public GameObject Trigger;
+
+    public bool ContinuouslyMoving = false;
+
+    void Start()
+    {
+        WayPointLength = Waypoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, Waypoints[Marker].transform.position, speed * Time.deltaTime);
+
+
+        if (!ContinuouslyMoving)
+        {
+
+            if (transform.position == Waypoints[Marker].transform.position & Trigger.GetComponent<PowerSource>().energised == true)
+            {
+                if (Marker < WayPointLength - 1)
+                {
+                    Marker++;
+                }
+            }
+            if (Marker == Waypoints.Length - 1 & Trigger.GetComponent<PowerSource>().energised == false)
+            {
+                Marker = 0;
+            }
+        }
+        else
+        {
+            if (Trigger.GetComponent<PowerSource>().energised == true)
+            {
+                if (transform.position == Waypoints[Marker].transform.position)
+                {
+                    Marker++;
+                }
+                if (Marker == Waypoints.Length)
+                {
+                    Marker = 0;
+                }
+
+            }
+        }
+    }
+}
+
+
+
+
+
