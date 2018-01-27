05e71ee5ce6729c96ce2fc7bbc2c6db4edb14204
Laptop\Wayne <waynehelley@outlook.com>
2018-01-27 15:57:23 +0000

    Various scripts.

--- Assets/_Scripts/PowerSource.cs
+++ Assets/_Scripts/PowerSource.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class PowerSource : MonoBehaviour {
 
     public bool energised;
-    bool levelGoal;
+    public bool levelGoal;
 
 	// Use this for initialization
 	void Start ()
     {	
             transform.GetChild(0).gameObject.SetActive(energised);
 	}
+
+    public void Energise()
+    {
+
+        energised = true;
+        transform.GetChild(0).gameObject.SetActive(true);
+
+        if(levelGoal)
+        {
+            //exit level logic
+        }
+
+    }
 	
 }
