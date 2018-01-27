cd48cb047fa096652e7392ffe561df16315f1a5d
waynehelley <waynehelley@hotmail.co.uk>
2018-01-27 06:47:40 +0000

    Direction Strings added for each IsTrigger collider.

--- Assets/_Scripts/PlayerDetectionLogic.cs
+++ Assets/_Scripts/PlayerDetectionLogic.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDetectionLogic : MonoBehaviour {
+    
+    //public GameObject ConnectionNoise;
+    private bool detection = false;
+    public string direction;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        //ConnectionNoise = GameObject.FindGameObjectWithTag("CoinCollectNoise");
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+
+            PlayerCircuitLogic otherPlayerCircuit = other.GetComponentInChildren<PlayerCircuitLogic>();
+
+            if (true)
+            {
+
+                //logic here
+
+                //AudioSource audio = ConnectionNoise.GetComponent<AudioSource>();
+                //audio.Play();
+            }
+            
+        }
+
+    }
+
 }
