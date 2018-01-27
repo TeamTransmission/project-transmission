f3eb883202d084ad4761881403b7cfb26bc5defe
vjos <joshjv@hotmail.co.uk>
2018-01-27 11:02:59 +0000

    Animation stuff
    Adding sprites, 3 animations and a controller

---
+++ Assets/_Scripts/PlayerAnimController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimController : MonoBehaviour {
+    Animator anim;
+    SpriteRenderer sr;
+    Rigidbody2D rb;
+
+    public double xvel;
+    public double yvel;
+
+    // Use this for initialization
+    void Start () {
+        
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        xvel = rb.velocity.x;
+        yvel = rb.velocity.y;
+
+        if (rb.velocity.y == 0)
+        {
+            anim.SetBool("jumping", false);
+        }
+        else
+        {
+            anim.SetBool("jumping", true);
+        }
+
+        if (rb.velocity.x < 0)
+        {
+            anim.SetBool("walking", true);
+            sr.flipX = true;
+        }
+        else if (rb.velocity.x > 0)
+        {
+            anim.SetBool("walking", true);
+            sr.flipX = false;
+        }
+        else
+        {
+            anim.SetBool("walking", false);
+        }
+    }
+}
