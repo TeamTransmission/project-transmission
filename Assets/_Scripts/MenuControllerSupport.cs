bf4c40fdbee2a33a3bea8465eb892749bebe241b
Laptop\Wayne <waynehelley@outlook.com>
2018-01-28 17:48:17 +0000

    Menu controller support started but not functioning.

--- Assets/_Scripts/MenuControllerSupport.cs
+++ Assets/_Scripts/MenuControllerSupport.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.UI;
 
 public class MenuControllerSupport : MonoBehaviour {
+     
+    private bool controllerSupportActive = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private GameObject playButton;
+    private GameObject quitButton;
+    private GameObject audioButton;
+    private GameObject helpButton;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if(Input.GetButtonDown("xbox button a") || Input.GetButtonDown("xbox button start") || Input.GetAxis("Horizontal") + Input.GetAxis("HorizontalGamePad") != 0)
+        {
+
+            //if this is the first time this runs, find the button gameObjects
+            if(!controllerSupportActive)
+            {
+                //playButton = transform.GetChild(0).GetChild(0).GetComponent<Button>();
+            }
+
+            Debug.Log("Button press");
+            controllerSupportActive = true;
+
+
+
+        }
+
+
+    }
 }
